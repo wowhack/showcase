d97b984e8113432c8278181f21d6a378f5193f8d
Johan Brook <johan.jvb@gmail.com>
2014-08-13 21:55:48 +0200

    Fix watching not working

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,15 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    watch = require('gulp-watch')
     minify = require('gulp-minify-css'),
     path = require('path'),
     app = require('./app'),
     gutil = require('gulp-util')
 
-var paths = {
-  css: gulp.dest('public/css'),
-  scss: gulp.src('public/stylesheets/*.scss')
-}
-
 var buildSass = function() {
-  paths.scss
-    .pipe(sass({
-      outputStyle: 'compressed'
-    }))
+  return gulp.src('public/stylesheets/*.scss')
+    .pipe(sass())
     .pipe(minify({ cache: true }))
-    .pipe(paths.css)
+    .pipe(gulp.dest('public/css'))
 }
 
 gulp.task('default', ['serve'])
@@ -35,10 +27,5 @@ gulp.task('server', function() {
 })
 
 gulp.task('watch', function() {
-  paths.scss
-    .pipe(watch())
-    .pipe(sass({
-      errLogToConsole: true
-    }))
-    .pipe(paths.css)
+  return gulp.watch('public/stylesheets/*.scss', ['css'])
 })
