0dc971b592f63eb0a4c1b9be4ede0326feab2f14
Johan Brook <johan.jvb@gmail.com>
2014-08-14 01:10:49 +0200

    Unslugify repo names

--- view-helpers.js
+++ view-helpers.js
@@ -4,9 +4,9 @@ module.exports = {
   cleanUrl: function(url) {
     return url && url.replace(/https?:\/\//, '')
   },
-  contentOr: function(str, opt) {
-    str = Handlebars.Utils.escapeExpression(str)
-    opt = Handlebars.Utils.escapeExpression(opt)
-    return str ? str : new Handlebars.SafeString('<span class="no-content">'+opt+'</span>')
+  unSlugify: function(input) {
+    return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase().replace(/[-|_](.)/g, function(match, group1) {
+      return ' ' + group1.toUpperCase()
+    })
   }
 }
